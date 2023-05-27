3de8af186a20048e02287be71afbf07516db9811
Benson <a283835639@gmail.com>
2023-05-27 16:18:52 +0800

    update: swagger設定
    Related work Item:
    
    1. 改使用 8000 port

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import { setSecurityHeaders } from './config/contentSecurityPolicy';
 import { apiRouter, itemsRouter } from './router/index';
-import swaggerSpec from '../swagger_output.json';
 import {
   handle404Error,
   handleErrors,
   handleUncaughtException,
   handleUnhandledRejection,
 } from './middlewares/error.middleware';
+import swaggerSpec from '../swagger_output.json';
 
 export const app = express();
 
