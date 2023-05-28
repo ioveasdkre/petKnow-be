adacad8692009fa0dcd8d8ee4865e5ad94ff98f7
Benson <a283835639@gmail.com>
2023-05-28 17:38:59 +0800

    update: 變數命名調整
    Related work Item:
    
    1. 金流 service

--- src/services/goldFlow.service.ts
+++ src/services/goldFlow.service.ts
@@ -4,7 +4,7 @@ import { CourseHierarchy, PlatformCoupons } from '../connections/mongoDB';
 import { Level } from '../enums/courseHierarchy.enums';
 
 class GoldFlowService {
-  async postCard(coursesIds: string[]) {
+  async postCardAsync(coursesIds: string[]) {
     const currentDate = new Date();
 
     const [courseHierarchy] = await CourseHierarchy.aggregate([
@@ -150,7 +150,7 @@ class GoldFlowService {
     return { courseHierarchy, youMightLike };
   }
 
-  async postCoupon(coursesIds: string[], couponCode: string) {
+  async postCouponAsync(coursesIds: string[], couponCode: string) {
     const [courseHierarchy] = await CourseHierarchy.aggregate([
       {
         $match: {
@@ -195,6 +195,152 @@ class GoldFlowService {
     ]);
 
     return platformCoupons.price;
+  }
+
+  async postCheckAsync(coursesIds: string[]) {
+    const currentDate = new Date();
+
+    const [courseHierarchy] = await CourseHierarchy.aggregate([
+      {
+        $match: {
+          $and: [
+            { _id: { $in: coursesIds.map(id => new Types.ObjectId(id)) } },
+            { isPublished: true }, // 判斷已上架
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'user',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalPrice: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ['$isFree', true], // 判斷免費課程
+                },
+                0,
+                {
+                  $cond: [
+                    {
+                      $and: [
+                        { $ifNull: ['$discountDate', false] }, // 判斷特價日期不為空
+                        { $gte: ['$discountDate', currentDate] }, // 判斷特價日期大於等於今天
+                      ],
+                    },
+                    '$discountPrice',
+                    '$price',
+                  ],
+                },
+              ],
+            },
+          },
+          shoppingCart: {
+            $push: {
+              _id: '$_id',
+              title: '$title',
+              cover: { $concat: [coverUrl, '$cover'] },
+              level: {
+                $switch: {
+                  branches: Object.entries(Level).map(([level, levelName]) => ({
+                    case: { $eq: ['$level', parseInt(level)] },
+                    then: levelName,
+                  })),
+                  default: null,
+                },
+              },
+              time: { $round: [{ $divide: ['$totalTime', 3600] }, 1] },
+              total: '$totalNumber',
+              instructorName: { $arrayElemAt: ['$user.name', 0] },
+              price: '$price',
+              discountPrice: {
+                $cond: [
+                  {
+                    $and: [
+                      { $ifNull: ['$discountDate', false] }, // 判斷特價日期不為空
+                      { $gte: ['$discountDate', currentDate] }, // 判斷特價日期大於等於今天
+                    ],
+                  },
+                  '$discountPrice',
+                  null,
+                ],
+              },
+              isFree: '$isFree',
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0, // 排除 _id 欄位
+          totalPrice: 1,
+          shoppingCart: 1,
+        },
+      },
+    ]);
+
+    const youMightLike = await CourseHierarchy.aggregate([
+      {
+        $match: {
+          $and: [
+            { isPublished: true, isPopular: true }, // 判斷已 上架 且 為熱門課程
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'user',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $sample: { size: 10 }, // 隨機讀取10筆
+      },
+      {
+        $project: {
+          _id: '$_id',
+          title: '$title',
+          cover: { $concat: [coverUrl, '$cover'] },
+          level: {
+            $switch: {
+              branches: Object.entries(Level).map(([level, levelName]) => ({
+                case: { $eq: ['$level', parseInt(level)] },
+                then: levelName,
+              })),
+              default: null,
+            },
+          },
+          time: { $round: [{ $divide: ['$totalTime', 3600] }, 1] },
+          total: '$totalNumber',
+          instructorName: { $arrayElemAt: ['$user.name', 0] },
+          price: '$price',
+          discountPrice: {
+            $cond: [
+              {
+                $and: [
+                  { $ifNull: ['$discountDate', false] }, // 判斷特價日期不為空
+                  { $gte: ['$discountDate', currentDate] }, // 判斷特價日期大於等於今天
+                ],
+              },
+              '$discountPrice',
+              null,
+            ],
+          },
+          isFree: '$isFree',
+        },
+      },
+    ]);
+
+    return { courseHierarchy, youMightLike };
   }
 }
 
