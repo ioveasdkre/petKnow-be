b811b7925070e9ea8da523517979af219e8a8a5f
Benson <a283835639@gmail.com>
2023-05-28 12:09:07 +0800

    update: 驗證相關整合
    Related work Item:
    
    1. 移除不必要檔案
    2. 整合程式碼

--- src/router/goldFlow.router.ts
+++ src/router/goldFlow.router.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express';
 import { GoldFlowController as controller } from '../controllers/goldFlow.controller';
-import { isValidObjectIds } from '../middlewares/verifyType.middewaes';
+import { verifyObjectIds } from '../middlewares/verifyType.middewaes';
 
 const router: Router = express.Router();
 
 router
-  .post('/v1/goldFlow/Card', isValidObjectIds, controller.postCard)
-  .post('/v1/goldFlow/QueryCoupon', isValidObjectIds, controller.postCoupon);
+  .post('/v1/goldFlow/Card', verifyObjectIds, controller.postCard)
+  .post('/v1/goldFlow/QueryCoupon', verifyObjectIds, controller.postCoupon);
 
 export { router as goldFlowRouter };
