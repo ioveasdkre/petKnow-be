b916b65955cb580fdd88a89f824a9dcc66e387e8
Benson <a283835639@gmail.com>
2023-06-25 14:23:39 +0800

    update: 課程介紹 API
    Related work Item:
    
    1. 新增封面

--- src/services/home.service.ts
+++ src/services/home.service.ts
@@ -412,6 +412,7 @@ class HomeService {
           _id: 0, // 排除 _id 欄位
           userId: '$user._id',
           name: '$user.name',
+          cover: '$cover',
           courseIntroduction: '$description',
           instructors: '$user.instructors',
           title: '$title',
